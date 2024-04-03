e6d0278c6262ced51acec12ae9c689d00468413f
Erdem Ozdemir <erdemozproductions@gmail.com>
2024-04-03 14:46:11 -0400

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import '@/assets/styles/globals.css';
+
+export const metadata = {
+  title: 'Property Pulse | Find The Perfect Rental',
+  description: 'Find your dream rental property',
+  keywords:'rentals, find rentals, properties'
+}
+
+const MainLayout = ({children}) => {
+  return (
+    <html lang='en'>
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+    
+  )
+}
+
+export default MainLayout
