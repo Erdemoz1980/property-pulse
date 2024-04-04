489654bb03d25c74f5e712f8d3c8feffdf73d8be
Erdem Ozdemir <erdemozproductions@gmail.com>
2024-04-04 16:48:53 -0400

    navbar + property card ui

---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,13 @@
+'use client';
+
+const PropertyPage = () => {
+
+  return (
+    <div>
+      <h1 className='text-3xl'> Add Property Page</h1>
+     
+    </div>
+  )
+}
+
+export default PropertyPage 
---
+++ app/terms/page.jsx
@@ -0,0 +1,11 @@
+
+
+const TermsPage = () => {
+  return (
+    <section>
+      Welcome to Property Pulse! By accessing or using our website, you agree to abide by the following terms and conditions. Please take a moment to familiarize yourself with these terms, as they govern your use of our platform. These terms outline important information regarding your rights, responsibilities, and limitations when using our services. If you have any questions or concerns, feel free to reach out to us. Happy browsing!"
+    </section>
+  )
+}
+
+export default TermsPage
